e81c83aba5cb2f6f99920543ebd7d66ac929f6ba
Nahid <mahmud.nahid@gmail.com>
2023-03-10 19:21:19 -0500

    fix css path

--- stable_diffusion/streamlit_app.py
+++ stable_diffusion/streamlit_app.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     # Create the page title 
     st.set_page_config(page_title='Diffusion Model generator', page_icon='fig/favicon.ico')
     # Create page layout
-    with open('style.css') as f:
+    with open('./style.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     st.title('Image generator using Stable Diffusion')
     st.caption('An app to generate images based on text prompts with a :blue[_Stable Diffusion_] model :sunglasses:')
